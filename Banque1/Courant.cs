68941ca6ae893c8c5d5a047bd6efe9e437efd183
Coline Ducourtieux <coline.ducourtieux@gmail.com>
2022-08-19 10:36:35 +0200

    initial commit

---
+++ Banque1/Courant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilRouge_Banque
+{
+    public class Courant
+    {
+        //Les propriétés publiques
+
+        private double _lignecredit;
+        private double _solde;
+        public string Numero { get; set; }
+        public double Solde { get; private set; } //Ajout d'une condition possible : Mon solde doit toujours être supérieur à ma limite solde+ligne de crédit.
+        public double LigneDeCredit
+        {
+            get { return _lignecredit; } //permet d'afficher le montant de la ligne de crdit (découvert)
+            set
+            {
+                if (value < 0) _lignecredit = 0; //Une autorisation à découvert est forcément = ou supérieure à zéro!
+                else _lignecredit = value;
+            }
+
+        }
+        public Personne Titulaire;
+
+
+        //        Les méthodes publiques :
+
+
+
+        public void Retrait(double Montant)
+        {
+            if (Montant > 0 && Montant <= _solde + _lignecredit) _solde = _solde - Montant;
+
+        }
+
+        public void Depot(double Montant)
+        {
+            if (Montant > 0) _solde = _solde + Montant;
+
+        }
+    }
+
+
+}
---
+++ Banque1/Personne.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilRouge_Banque
+{
+    public class Personne
+    {
+
+        //#region Champs - Variables membres
+        //private string _nom;
+        //private string _prenom;
+        //private DateTime _dateNaissance;
+        //#endregion
+
+        #region Propriétés
+        //public string Nom
+        //{
+        //    get { return _nom; }
+        //    set
+        //    {
+        //        if (!string.IsNullOrWhiteSpace(value)) _nom = value.Trim(); // permet de vérifier que la veleur entrée n'est pas nulle. value.Trim() permet de retirer tous les espaces vides avant et après l'entrée. 
+
+        //    }
+
+        public string Nom { get; set; } // une autopropritété est plus adaptée s'il n'y a pas de conditions à la propriété. (cf Courant.LigneDeCrédit).
+        public string Prenom { get; set; }
+        public DateTime DateNaissance { get; set; }
+        #endregion
+
+        #region Constructeur & Destructeur
+        #endregion
+
+        #region Méthodes et opérateurs
+        #endregion
+
+    }
+}
